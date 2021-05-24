be660f2adf07a2700a0e99519291efdb82bd1f99
thramania <thramania@hanmail.net>
2021-05-24 21:57:27 +0900

    Fourth commit

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = event => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = event => {
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <form>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        ></input>
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        ></input>
+        <input type="submit" value="Login"></input>
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 export default Auth;
